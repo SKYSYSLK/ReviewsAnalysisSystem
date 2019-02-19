da058b10f245b226b3f51a112e15a8836994921b
RavinduSachintha <ravindusachintha53@gmail.com>
2019-02-19 20:47:51 +0530

    seeds updated

--- database/seeds/ReviewTableSeeder.php
+++ database/seeds/ReviewTableSeeder.php
@@ -11,16 +11,6 @@ class ReviewTableSeeder extends Seeder
      */
     public function run()
     {
-        $items = [
-            ['user_id'=>'2','category_id'=>'1','review' => 'wow, this is awesome', 'score' => '0.2', 'level' => '2'],
-            ['user_id'=>'2','category_id'=>'1','review' => 'WTF', 'score' => '-0.1', 'level' => '4'],
-            ['user_id'=>'2','category_id'=>'2','review' => 'I dont like this shit', 'score' => '-0.5', 'level' => '4'],
-            ['user_id'=>'2','category_id'=>'3','review' => 'I dont like this shit', 'score' => '0.1', 'level' => '3'],
-            ['user_id'=>'2','category_id'=>'4','review' => 'I dont like this shit', 'score' => '0.8', 'level' => '1'],
-            ['user_id'=>'2','category_id'=>'4','review' => 'Great Service', 'score' => '0.9', 'level' => '4'],
-        ];
-        foreach ($items as $item) {
-            \App\Review::create($item);
-        }
+        //
     }
 }
